a16289fa3bf24d88bd1ae6d3a6bd5c773f3be406
Stephan Guilloux <stephan.guilloux@crisalid.com>
2018-08-13 10:16:39 +0200

    Differentiate C and GO variables (useful when adding traces).

--- util.go
+++ util.go
@@ -29,8 +29,12 @@ func zHashToMap(zhash *C.struct__zhash_t, free bool) map[string]string {
 	keyVal := make(map[string]string)
 	for item := C.zhash_first(zhash); item != nil; item = C.zhash_next(zhash) {
 		ckey := C.zhash_cursor(zhash)
-		cval := C.GoString((*C.char)(C.zhash_lookup(zhash, ckey)))
-		keyVal[C.GoString(ckey)] = cval
+		key := C.GoString((*C.char)(ckey))
+
+		cval := C.zhash_lookup(zhash, ckey)
+		val := C.GoString((*C.char)(cval))
+
+		keyVal[key] = val
 	}
 	return keyVal
 }
